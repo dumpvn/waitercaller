a62f2f864f2da1bc437dc1f9eb46d9a607aceee1
Phuc Nguyen <phuc.vinh@outlook.com>
2019-09-07 09:04:46 +0700

    updated code

--- waitercaller.py
+++ waitercaller.py
@@ -3,18 +3,18 @@ from flask import Flask
 from flask import request
 from flask.ext.login import (login_user, logout_user)
 from mockdbhelper import MockDBHelper
+from passwordhelper import PasswordHelper
 
-# should be moved into app package
-# under def create_app(config)
 app = Flask(__name__)
 DB = MockDBHelper()
+PH = PasswordHelper()
 
 @app.route("/login", methods=["POST"])
 def login():
     email = request.form.get("email")
     password = request.form.get("password")
-    user_password = DB.get_user(email)
-    if user_password and user_password == password:
+    stored_user = DB.get_user(email)
+    if stored_user and PH.validate_password(password, stored_user['salt'], stored_user['hashed']):
         user = User(email)
         login_user(user, remember=True)
         return redirect(url_for('account'))
